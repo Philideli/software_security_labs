da11dbae76b0ab976273c718d9b9fda81444e472
Kseniia Ivanchenko <k.ivanchenko-2027@kpi.ua>
2023-12-25 20:12:02 +0100

    update lab 5 and lab 6

--- Lab6/index.js
+++ Lab6/index.js
@@ -12,13 +12,13 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.redirect(
-        'https://dev-lx288wyj7b3ybbcr.us.auth0.com/authorize?client_id=Ft75xpLvH9wfRfOIbcOT03UfzaL28RIE&redirect_uri=http://localhost:3000/callback&response_type=code&response_mode=query',
+        'https://dev-tvfcmdxn6j7sl5k0.us.auth0.com/authorize?client_id=KTLXy6HNZuVdotdT4CA6TBvICQa6TTcT&redirect_uri=http://localhost:3000/callback&response_type=code&response_mode=query',
     );
 });
 
 app.get('/logout', (req, res) => {
     res.redirect(
-        'https://dev-lx288wyj7b3ybbcr.us.auth0.com/v2/logout?returnTo=http://localhost:3000&client_id=Ft75xpLvH9wfRfOIbcOT03UfzaL28RIE',
+        'https://dev-tvfcmdxn6j7sl5k0.us.auth0.com/v2/logout?returnTo=http://localhost:3000&client_id=KTLXy6HNZuVdotdT4CA6TBvICQa6TTcT',
     );
 });
 
@@ -34,15 +34,15 @@ app.get('/callback', (req, res) => {
 
     const authorizationCodeData = {
         method: 'post',
-        url: `https://dev-lx288wyj7b3ybbcr.us.auth0.com/oauth/token`,
+        url: `https://dev-tvfcmdxn6j7sl5k0.us.auth0.com/oauth/token`,
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
         data: {
             grant_type: 'authorization_code',
-            audience: 'https://dev-lx288wyj7b3ybbcr.us.auth0.com/api/v2/',
-            client_id: 'Ft75xpLvH9wfRfOIbcOT03UfzaL28RIE',
-            client_secret: 'ZbAgOplUESLfwPmTKV6OkxgcLmsK7_k-jn3ny_yuMvrCMWY1c4Xe8acYAMHC-sq7',
+            audience: 'https://dev-tvfcmdxn6j7sl5k0.us.auth0.com/api/v2/',
+            client_id: 'KTLXy6HNZuVdotdT4CA6TBvICQa6TTcT',
+            client_secret: '69eUyZmTDZSPiJbjZltKOC_a2psTXjMAWhNYNWwsQyHy-zuvvEDrKqCOnU-ImNVA',
             code: code,
             redirect_uri: 'http://localhost:3000/callback',
         },
